a1996e36bc97395ca79af6ba91e1472c413aca60
Richard Kurwitz <kurwitz3@aol.com>
2020-09-06 00:15:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,7 +30,7 @@ end
  end
  end
  def list_artists
-   Song.all.sort_by(&:name.each.with_index(1) do |x,z|
+   Song.all.sort_by(&:name).each.with_index(1) do |x,z|
     binding.pry 
      puts "#{z}. #{x.artist.name}"
    end
