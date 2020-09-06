68abed5c568164764cf3579cd32a7b4720baf6f8
Richard Kurwitz <kurwitz3@aol.com>
2020-09-06 02:16:29 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,6 +67,7 @@ end
 def play_song 
  puts "Which song number would you like to play?"
   song = gets.chomp.to_i
+  binding.pry 
   if list_songs.include?(song)
   puts "PLaying #{number.name} by #{number.artist.name}"
 end 
