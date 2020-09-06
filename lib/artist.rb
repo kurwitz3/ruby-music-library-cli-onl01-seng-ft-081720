51dcb1822582373eda225b2cfe5d4e38bc6cd271
Richard Kurwitz <kurwitz3@aol.com>
2020-09-06 00:11:51 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+
+require 'pry'
+class Artist 
+  extend Concerns::Findable 
+  
+  attr_accessor :name,:song
+  attr_reader :songs
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+    save 
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  def self.destroy_all 
+    @@all.clear 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+  end
+  
+  
+  def add_song(song)
+    if song.artist == nil 
+       song.artist = self
+    @songs << song
+  end
+ end 
+  
+def genres
+  new = []
+   Song.all.collect do |x|
+    if x.artist == self 
+     new << x.genre
+    end
+  end
+ new.uniq
+ end
+
+end 
---
+++ lib/genre.rb
@@ -0,0 +1,45 @@
+class Genre 
+  extend Concerns::Findable 
+  attr_accessor :name
+   @@all = [] 
+   
+   def initialize(name)
+     @name = name 
+     @songs =[]
+     save
+      end 
+  
+  def songs 
+    @songs
+  end
+ 
+   def self.all 
+    @@all 
+  end 
+ 
+def save 
+  @@all << self 
+end 
+
+def self.destroy_all 
+     @@all.clear 
+   end 
+   
+   def self.create(name)
+      genre = new(name)
+      genre.save
+      genre
+  end
+  
+  def artists
+  new = []
+   Song.all.collect do |x|
+    if x.genre == self 
+     new << x.artist
+    end
+  end
+ new.uniq
+ end
+  
+  
+ end 
